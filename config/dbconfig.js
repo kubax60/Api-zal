accf3487bfc7acfa612eca7f99c62903644d5df0
Jakub <80392066+kubax60@users.noreply.github.com>
2022-01-27 22:17:13 +0100

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'nictuniema',
-    database: 'yourdburlheremongodb+srv://admin:<password>@cluster0.syerz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    database: 'mongodb+srv://admin:<password>@cluster0.syerz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
